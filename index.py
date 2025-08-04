7b2324a42e5ae01d5c82025ff1450a53c4c92831
Talksick-Codes <hudsonmabera2005@gmail.com>
2025-08-04 09:35:11 +0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,11 @@
+print("Creation of a simple Calculator")
+num1=int(input("Enter number 1: "))
+num2=int(input("Enter number 2: "))
+Addition=num1+num2
+Subtraction=num2-num1
+Division=num2/num1
+Multiplication=num2*num1
+print(f"{num1}+{num2}={Addition}")
+print(f"{num2}-{num1}={Subtraction}")
+print(f"{num2}/{num1}={Division}")
+print(f"{num2}*{num1}={Multiplication}")
